8bf7b2405661f36bc7ba45dedbf494dc9ad68edf
Sudip1988 <36604130+Sudip1988@users.noreply.github.com>
2018-06-30 21:16:28 -0700

    commit

--- TaskMaster.Data/Repository.cs
+++ TaskMaster.Data/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,74 +9,67 @@ using TaskMaster.Data;
 
 namespace TaskMaster.Data
 {
+    [ExcludeFromCodeCoverage]
     public class Repository: IRepository
     {
+        private TaskManagerEntities _db;
+        public Repository()
+        {
+            TaskManagerEntities db = new TaskManagerEntities();
+            _db = db;
+        }
+        public Repository(TaskManagerEntities db)
+        {
+            _db = db;
+        }
         public void AddTask(Task_Table task)
         {
-            using (var db = new TaskManagerEntities())
-            {
+            
+                _db.Task_Table.Add(task);
 
-                db.Task_Table.Add(task);
-
-                db.SaveChanges();
+                _db.SaveChanges();
 
 
-            }
         }
 
         public IList<Task_Table> GetTasks()
         {
-            using (var db = new TaskManagerEntities())
-            {
-                return db.Task_Table.Include("ParentTask_Table").ToList();
-            }
+                return _db.Task_Table.Include("ParentTask_Table").ToList();
         }
 
 
 
         public Task_Table GetSpecificTask(int taskId)
         {
-            using (var db = new TaskManagerEntities())
-            {
-                return db.Task_Table.Include("ParentTask_Table").FirstOrDefault(x => x.Task_ID == taskId);
-            }
+                return _db.Task_Table.Include("ParentTask_Table").FirstOrDefault(x => x.Task_ID == taskId);
         }
 
         public void EndTask(int taskId)
         {
-            using (var db = new TaskManagerEntities())
-            {
-                var task = db.Task_Table.FirstOrDefault(x => x.Task_ID == taskId);
+                var task = _db.Task_Table.FirstOrDefault(x => x.Task_ID == taskId);
                 task.End_Date = System.DateTime.Now;
-                db.SaveChanges();
-            }
+                _db.SaveChanges();
         }
 
         public void DeleteTask(int taskId)
         {
-            using (var db = new TaskManagerEntities())
-            {
-                var task = db.Task_Table.FirstOrDefault(x => x.Task_ID == taskId);
+                var task = _db.Task_Table.FirstOrDefault(x => x.Task_ID == taskId);
                 if (task != null)
                 {
-                    db.Task_Table.Remove(task);
-                    db.SaveChanges();
+                    _db.Task_Table.Remove(task);
+                    _db.SaveChanges();
                 }
-            }
 
 
         }   
             
         public bool IsTaskExists(string taskName)
         {
-            using (var db = new TaskManagerEntities())
-            {
-                var task = db.Task_Table.FirstOrDefault(x => x.Task == taskName);
+                var task = _db.Task_Table.FirstOrDefault(x => x.Task == taskName);
                 if (task != null)
                     return true;
                 else
                     return false;
-            }
         }
 
     }
